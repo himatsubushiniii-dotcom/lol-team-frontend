47c97036ed616c22fbb341c8cbc7a4e45d16b227
gitshirakimizutani <gitshirakimizutani@gmail.com>
2025-10-26 13:17:50 +0900

    cx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoLTeamMaker from './lol_team_maker.tsx'
+import LoLTeamMaker from './lol_team_maker'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
